ed823f8c28463998b02d52a7e6bc0c9ad800790c
AndrewDo16 <Danilin1AV@yandex.ru>
2023-06-05 17:17:13 +0300

    new project

---
+++ src/test/java/ru/project/exampleRest/ExampleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.exampleRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
